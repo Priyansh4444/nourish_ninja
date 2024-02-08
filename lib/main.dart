829c7af3788094897a642f950c1f52357074a0f2
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-02-08 15:04:19 -0800

    learning how to use android emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
 
+  runApp( MyApp() );
+
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to Flutter!',
-          style: TextStyle(fontSize: 24),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          backgroundColor: Colors.redAccent,
         ),
-      ),
+        body: Text('This is my default text!'),
+      )
     );
   }
 }
