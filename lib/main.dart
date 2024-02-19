8268de65db7ee8db4b0d5a11b7bb2f8e86c4decf
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-02-18 22:18:42 -0800

    my ai is working, step 1 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,69 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> main() async {
-  // Access your API key as an environment variable (see "Set up your API key" above)
-  await dotenv.load(fileName: '.env');
-  final apiKey = dotenv.env['API_KEY'];
-  if (apiKey == null) {
-    print('No \$API_KEY environment variable');
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nourish Ninja',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
   }
-  // For text-only input, use the gemini-pro model
-  final model = GenerativeModel(
-      model: 'gemini-pro',
-      apiKey: apiKey!,
-      generationConfig: GenerationConfig(maxOutputTokens: 100));
-  // Initialize the chat
-  final chat = model.startChat(history: [
-    Content.text('Hello, I have 2 dogs in my house.'),
-    Content.model([TextPart('Great to meet you. What would you like to know?')])
-  ]);
-  var content = Content.text('How many paws are in my house?');
-  var response = await chat.sendMessage(content);
-  print(response.text);
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> _startChat() async {
+    await dotenv.load();
+    final apiKey = dotenv.env['API_KEY'];
+    if (apiKey == null) {
+      debugPrint('No \$API_KEY environment variable');
+      exit(1);
+    }
+    final model = GenerativeModel(
+        model: 'gemini-pro',
+        apiKey: apiKey,
+        generationConfig: GenerationConfig(maxOutputTokens: 100));
+    final chat = model.startChat(history: [
+      Content.text('Hello, I have 100 dogs in my house.'),
+      Content.model([TextPart('Great to meet you. What would you like to know?')])
+    ]);
+    var content = Content.text('How many paws are in my house?');
+    var response = await chat.sendMessage(content);
+    debugPrint(response.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nourish Ninja'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _startChat,
+          child: const Text('Start Chat'),
+        ),
+      ),
+    );
+  }
 }
